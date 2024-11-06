5f4c7f7051e8798b95684c8e3d68aa94fffa73dd
Sidney <sidney@emprise1.com>
2024-11-06 09:45:20 -0500

    Add vue app

---
+++ Personal-Vue/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!--<img alt="Seemly Sand logo" class="logo" src="./assets/SeemlySandLogo.svg" width="225" height="225" /> -->
+
+    <div class="wrapper">
+      <HelloWorld name="Sidney Sanders" msg = "Software Developer II with Emprise Technologies"/>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Personal-Vue/src/components/TheWelcome.vue
@@ -0,0 +1,68 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Education</template>
+    
+    Attended Bowling Green State University to attain a degree from their
+    <a href="https://www.bgsu.edu/academics/computer-science.html" target="_blank" rel="noopener">Computer Science bachelors</a>
+    program. with a minor in General Science. 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Job</template>
+
+    Retained a job with  
+    <a href="https://emprise1.com" target="_blank" rel="noopener">Emprise Technologies</a>
+    in the fall of 2022 as a software Developer. Achieved Software Developer II position in the fall of 23.
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Work</template>
+
+    Currently training with 
+    <a href="https://vuejs.org" target="_blank" rel="noopener">Vue </a>
+     tools and tutorials to make best use of downtime between project assignments. 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hobbies</template>
+
+    Participates in a variety of activities including team member of company employee 
+    organized volleyball league in downtown Toledo at The Dock, and playing through 
+    the beta game 
+    <a href="https://palia.com/" target="_blank" rel="noopener">Palia</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Support</template>
+
+    As an independent project, Vue relies on community backing for its sustainability. You can help
+    us by
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+  </WelcomeItem>
+</template>
